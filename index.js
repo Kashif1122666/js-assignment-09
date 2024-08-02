7db30de2b2f39a45ad8ddfea42f373f60df88ec2
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-08-02 16:10:57 -0700

    Add files via upload
    done

---
+++ index.js
@@ -0,0 +1,67 @@
+// EXCERCISE ONE:
+// get input from user and show  table of it
+
+// const userinpt = +prompt("please enter any number to get its table");
+// for(var i = 1; i<=10; i++){
+//     var result = userinpt * i;
+//     console.log(`${userinpt}*${i}=${result}`);    
+// }
+// ========================================
+
+// EXCERCISE TWO:
+// five number sum
+
+// var arrayy = [];
+// var sum = 0;
+// for(var i = 0; i < 5; i++){
+//     const userinput = +prompt("enter a number");
+//     arrayy.push(userinput);
+//   console.log(`${i}input.  ${arrayy[i]}`); 
+//       sum += userinput;
+// }
+//   console.log(`sum of all inputs ${sum}`);
+// ===============================================
+
+// EXCERCISE THREE:
+// Highest number in given numbers
+
+// var arrayy = [];
+// for(var i = 0; i<5; i++){
+//     var userinpt = +prompt("please input any number ");
+//     arrayy.push(userinpt);
+// }
+// let Highest = arrayy[0];
+// for(var i = 0; i < arrayy.length; i++){
+//     if(arrayy[i] > Highest ){
+//        Highest = arrayy[i];
+//     }
+// }
+// console.log(arrayy);
+// console.log(`Highest number from the given array ${Highest}`);
+// ==================================================
+
+// EXCERCISE FOUR:
+// Get an input from user as a string and display it in opposite direction
+
+// let userinpt = prompt("please enter any string")
+//    let reversed_order = "";
+// for(var i = userinpt.length-1; i>=0; i--){
+//       reversed_order += userinpt[i];     
+// };
+// console.log(`user input:      ${userinpt}`);
+// console.log(`reversed_order:  ${reversed_order}`); 
+// =================================================
+
+// EXCERCISE FIVE:
+// Get a Number from user and find factiorial of it
+
+// var userinpt = +prompt("please enter any number");
+// var factorial = 1;
+// for(var i = userinpt-1; i >=0; i--){
+   
+//  factorial += factorial*i;
+ 
+// }
+// console.log(factorial);
+// ==============================
+
